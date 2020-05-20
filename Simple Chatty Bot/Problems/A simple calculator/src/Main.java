8f67368f717d4b97e9e1fdb7c6a5ee01438278de
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-19 20:11:31 -0500

    Create a simple calculator that reads 3 inputs

---
+++ Simple Chatty Bot/Problems/A simple calculator/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        long num1 = scanner.nextLong();
+        String operator = scanner.next();
+        long num2 = scanner.nextLong();
+
+        switch (operator) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Division by 0!");
+                } else {
+                    System.out.println(num1 / num2);
+                }
+                break;
+            default:
+                System.out.println("Unknown operator ");
+        }
+    }
+}
