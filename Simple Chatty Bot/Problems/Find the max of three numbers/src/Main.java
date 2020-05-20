aac32f02639e9e8fc231d5fa8251387238f5fe14
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-19 20:21:14 -0500

    Return the index of the largest parameter

---
+++ Simple Chatty Bot/Problems/Find the max of three numbers/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int getNumberOfMaxParam(int a, int b, int c) {
+        // write a body here
+        if (a >= b && a >= c) {
+            return 1;
+        }
+        if (b >= a && b >= c) {
+            return 2;
+        }
+        if (c >= a && c >= b) {
+            return 3;
+        }
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+        final int c = scanner.nextInt();
+
+        System.out.println(getNumberOfMaxParam(a, b, c));
+    }
+}
