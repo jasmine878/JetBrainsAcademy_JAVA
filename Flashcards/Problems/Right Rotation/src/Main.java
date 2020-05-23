c59cede2541560d73c38a4f72a07b5f4fc86cdf3
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-22 22:32:03 -0500

    Rotate an array based on the user input

---
+++ Flashcards/Problems/Right Rotation/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        
+        String[] arr = scanner.nextLine().split(" ");
+        int size = arr.length;
+        int rotation = scanner.nextInt() % size;
+        String[] rotatedArr = new String[size];
+        int shifts = 0;
+        int shifts2 = 0;
+        
+        for (int i = 0; i < rotation; i++) {
+            rotatedArr[i] = arr[size - rotation + shifts];
+            shifts++;
+        }
+        
+        for (int i = shifts; i < size; i++) {
+            rotatedArr[i] = arr[0 + shifts2];
+            shifts2++;
+        }
+        
+        System.out.println(String.join(" ", rotatedArr));
+    }
+}
