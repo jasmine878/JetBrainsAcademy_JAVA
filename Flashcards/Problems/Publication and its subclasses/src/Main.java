197f4dcbde9853f91a08f4a05d42743cda27f780
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-27 22:17:23 -0500

    Create a Publication class and methods

---
+++ Flashcards/Problems/Publication and its subclasses/src/Main.java
@@ -0,0 +1,65 @@
+class Publication {
+    public static void main(String[] args) {
+        Publication publication = new Publication("The new era");
+        System.out.println(publication.getInfo());
+    }
+
+    private String title;
+
+    public Publication(String title) {
+        this.title = title;
+    }
+
+    public final String getInfo() {
+        // write your code here
+        return this.getType() + this.getDetails() + ": " + title;
+    }
+
+    public String getType() {
+        return "Publication";
+    }
+
+    public String getDetails() {
+        return "";
+    }
+
+}
+
+class Newspaper extends Publication {
+
+    private String source;
+
+    public Newspaper(String title, String source) {
+        super(title);
+        this.source = source;
+    }
+
+    // write your code here
+
+}
+
+class Article extends Publication {
+
+    private String author;
+
+    public Article(String title, String author) {
+        super(title);
+        this.author = author;
+    }
+
+    // write your code here
+
+}
+
+class Announcement extends Publication {
+
+    private int daysToExpire;
+
+    public Announcement(String title, int daysToExpire) {
+        super(title);
+        this.daysToExpire = daysToExpire;
+    }
+
+    // write your code here
+
+}
